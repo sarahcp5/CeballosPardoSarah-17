52da2f8230f702910d9435288bce54dc1a488032
Sarah Ceballos Pardo <sceballospardo@mingeneros.gob.ar>
2022-10-27 21:29:04 -0300

    Desplegar nuestro proyecto en la nube

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const server = app.listen(PORT, () => {
     console.log(`Servidor HTTP escuchando en el puerto ${server.address().port}`);
