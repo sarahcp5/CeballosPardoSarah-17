dcf8a0787684a3073b14adf1fe6d27f5fd9d9878
Sarah Ceballos Pardo <sceballospardo@mingeneros.gob.ar>
2022-10-28 01:01:33 -0300

    Desplegar nuestro proyecto en la nube

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
-import path from 'path';
+import { dirname } from 'path'
 import { fileURLToPath } from 'url';
 import bcrypt from 'bcrypt';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 export const createHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 export const isValidPassword = (user, password) => bcrypt.compareSync(password, user.password);
